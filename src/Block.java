a19b788736139fbe50649ef28ad1840515863a28
Jerry Yang <z295yang@edu.uwaterloo.ca>
2017-01-22 09:31:03 -0500

    Version 0.0.1
    - Added image loading capabilities
    - Blocks now look nicer
    - Added Vector class

--- src/Block.java
+++ src/Block.java
@@ -3,17 +3,25 @@ import java.util.ArrayList;
 public class Block extends Item 
 {
 	private int mass;
+	private final String BLOCK_URL = "block";
 	public Block (int x, int y, int length, int height, int mass)
 	{
 		this.length = length;
 		this.height = height;
 		this.mass = mass;
 		points = new ArrayList <Point> ();
-		 
+		
 		points.add(new Point (x, y));
 		points.add(new Point (x + length, y));
 		points.add(new Point (x, y + height));
 		points.add(new Point (x + length, y + height));	
+		
+		url = BLOCK_URL;
+	    loadImage();
 	}
 	
+	public int getMass()
+	{
+		return mass;
+	}
 }
--- src/Item.java
+++ src/Item.java
@@ -1,16 +1,35 @@
-import java.awt.Color;
+
 import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 
 public abstract class Item 
 {
 	protected ArrayList <Point> points;
 	protected int length; 
 	protected int height;
+	protected String url;
+	protected Image image;
+	
+	public void loadImage ()
+	{
+		Image temp = null;
+		try
+	    {
+			temp = ImageIO.read (new File ("Images/" + url + ".png")); // load file into Image object     
+	    }
+	    catch (IOException e) // Catch exception if image does not exist
+	    {
+	    }
+		image = temp;
+	}
 	
 	public void show (Graphics g)
 	{
-		g.setColor(Color.LIGHT_GRAY);
-		g.fillRect(points.get(1).getX(), points.get(1).getY(), length, height);
+		g.drawImage(image, points.get(1).getX(), points.get(1).getY(), length, height, null);
 	}
 }
